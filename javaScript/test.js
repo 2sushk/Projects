e30713bb6cddae23f16fc5b203725b9b2e520208
sush <sushinguz@gmail.com>
2024-01-10 16:48:46 +0530

    uodated

--- javaScript/test.js
+++ javaScript/test.js
@@ -1,1 +1,10 @@
 console.log("Hello World, Welcome to the javascript basics");
+
+let age= "22";
+var name= "sush";
+accoutId = "501";
+const phone = "980";
+let ram;
+var abc= null;
+
+console.table([age, name,accoutId, phone,ram,abc]);
